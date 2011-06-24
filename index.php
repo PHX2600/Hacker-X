a37aa9ba7f2342f7a436f64e3fc06b062d501b8f
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2011-06-23 21:30:01 -0700

    Added post meta data and styled accordingly

--- index.php
+++ index.php
@@ -22,26 +22,46 @@
             
             <?php if (have_posts()): ?>
                 <?php while (have_posts()): ?>
+                    
+                    <?php the_post(); ?>
+                    
                     <div class="contentBox">
                         
-                        <?php // if (the_title()): ?>
-                            <div class="titleWrapper clearfix">
-                                <h2 class="postTitle floatLeft">
-                                    <a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title_attribute(); ?>">
-                                        <?php the_title(); ?>
-                                    </a>
-                                    
-                                </h2>
-                                <?php if(current_user_can('edit_post', $post->ID)): ?>
-                                    <span class="postEdit"><?php edit_post_link('Edit', '', ''); ?></span>
-                                <?php endif; ?>
-                            </div>
-                        <?php // endif; ?>
+                        <div class="titleWrapper clearfix">
+                            <h2 class="postTitle floatLeft">
+                                <a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title_attribute(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+                            
+                            <?php if(current_user_can('edit_post', $post->ID)): ?>
+                                <a href="<?php get_edit_post_link($post->ID); ?>" class="editLink">Edit</a>
+                            <?php endif; ?>
+                        </div>
                         
-                                <?php the_post(); the_content(); ?>
+                        <?php the_content(); ?>
+                        
+                        <div class="postMetaData">
+                            <div class="metaDataTop">
+                                Posted by, <?php the_author_posts_link() ?>
+                                on <?php the_time(get_option('date_format')); ?>
+                                at <?php the_time(); ?> |
+                                <?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'postComments', 'Comments disabled'); ?>
+                            </div>
+                            
+                            <div class="metaDataBottom">
+                                <span class="postCategory">Category: <?php the_category(' &bull; ') ?></span>
+                                <?php the_tags('<span class="postTags">Tags: ', ' &bull; ', '</span>'); ?>
+                            </div>
+                        </div>
                         
                     </div>
+                    
                 <?php endwhile; ?>
+            <?php else: ?>
+                <div class="contentBox">
+                    <p>Sorry, no posts matched your criteria.Sorry, no posts matched your criteria.</p>
+                </div>
             <?php endif; ?>
         </div>
     </div>
